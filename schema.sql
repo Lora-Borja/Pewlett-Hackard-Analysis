9cb9d4144c25f561c0a36325bd1c65ef3cf222da
Lora-Borja <73265259+Lora-Borja@users.noreply.github.com>
2020-12-12 16:57:16 -0500

    Updating schema.sql

--- schema.sql
+++ schema.sql
@@ -98,3 +98,50 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
+
+-- Recreating the retirement info table to include employee_no.
+DROP TABLE retirement_info;
+
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Check the new retirement info table.
+SELECT * FROM retirement_info;
+
+-- Practicing joining tables. Joining departments and dept_manager tables and creating aliases of table names.
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- Joining retirement_info and dept_emp tables; creating aliases of table names; creating new current_emp table.
+SELECT ri.emp_no,
+    ri.first_name,
+	ri.last_name,
+    de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_employees as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Check the new current_emp table.
+SELECT * FROM current_emp;
+
+-- Employee count by department number and putting that into a new table retirement_count_bydept.
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO retirement_count_bydept
+FROM current_emp as ce
+LEFT JOIN dept_employees as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+-- Check the new retirement_count_bydept table.
+SELECT * FROM retirement_count_bydept;

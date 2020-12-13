136b6727575f3d37b668b5302336b4c2375d3fe0
Lora-Borja <73265259+Lora-Borja@users.noreply.github.com>
2020-12-12 23:13:36 -0500

    Adding Queries/Employee_Database_challenge.sql

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,59 @@
+-- Deliverable 1: The Number of Retiring Employees by Title
+SELECT e.emp_no,
+    e.first_name,
+	e.last_name,
+    t.title,
+    t.from_date,
+    t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Check the new retirement titles table.
+SELECT * FROM retirement_titles;
+
+-- Use Distinct with Orderby to remove duplicate rows.
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+    rt.first_name,
+    rt.last_name,
+    rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+-- Check the unique titles table.
+SELECT * FROM unique_titles;
+
+-- The number of employees by their most recent title who are about to retire.
+SELECT COUNT(ut.emp_no), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY COUNT(ut.emp_no) DESC;
+
+-- Check the retiring titles table.
+SELECT * FROM retiring_titles;
+
+-- Deliverable 2: The Employees Eligible for the Mentorship Program
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+	e.last_name,
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_employees as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01')
+     AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+-- Check the mentorship eligibility table.
+SELECT * FROM mentorship_eligibility;
